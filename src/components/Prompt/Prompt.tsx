2c1109fced2142196af586c92eca6688dcb68ce1
Ihor Leonov <ihorleonow@gmail.com>
2024-03-11 09:12:28 +0200

    FIX | disable prompt

--- src/components/Prompt/Prompt.tsx
+++ src/components/Prompt/Prompt.tsx
@@ -4,15 +4,10 @@ import { PromptProps } from "../../constants/types";
 
 const balanceString = "Staking rewards get allocated on this sum";
 const aprString = "Displays the average for APR.";
-const aprSecondString =
-  "Interest rate is calculated for each amount of tokens.";
+const aprSecondString = "Interest rate is calculated for each amount of tokens.";
 const rewardsString = "Rewards get allocated every second";
 
 export const Prompt: FC<PromptProps> = ({ promptName, promptClass }) => {
-  // const prompt = document.getElementById("prompt");
-  // const rect = prompt?.getBoundingClientRect();
-  // console.log(rect);
-
   return (
     <div id="prompt" className={s.prompt + " " + promptClass}>
       <h3 className={s.prompt_title}>{promptName}</h3>
